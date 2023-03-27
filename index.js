ddb158f8c64c83aa7fc7756ba7a6f56a4d930ecf
Johana Ringelová <ringelovaj@gmail.com>
2023-03-27 18:19:35 +0200

    Řešení

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+let formElm = document.querySelector('#newsletter')
+
+    formElm.addEventListener('submit', (event) => {
+        
+        let emailInput = document.querySelector('#email')
+        let email = emailInput.value    
+        
+        event.preventDefault()
+
+        formElm.textContent = `Děkujeme za Váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese: ${email}.`
+
+    }) 
+
+let emailInput = document.querySelector('#email')
+let email = emailInput.value    
+
+let check = (event) => {
+
+    if (email === "" || email.includes("@") == false){
+    
+        emailInput.classList.add("email")
+
+    } else {
+
+        emailInput.classList.remove("email")
+    }
+}
+
+emailInput.addEventListener("input", check)
+
+
+/*
+
+Nedaří se mi odebrat červený rámeček poté, co uživatel napíše do políčka @ . 
+
+
+*/
